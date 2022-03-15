6204aaf3aba65fe9f273eaab61c1cb41cdd9e4fb
aswinthottilpalam <aswinp264@gmail.com>
2022-03-15 09:43:24 +0530

    Player at starting Position Zero

---
+++ PlayerStartingPositionZero.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProblem
+{
+    class PlayerStartingPositionZero
+    {
+        public static void snake()
+        {
+            int position = 0;
+            Console.WriteLine("Player is at position : " + position);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace SnakeAndLadderProblem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome To Snake Ladder Game");
+            PlayerStartingPositionZero.snake();
         }
     }
 }
